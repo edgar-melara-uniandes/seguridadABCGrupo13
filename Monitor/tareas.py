568222ab883ecbd1e7b57e969d5cddf466b1068f
Edgar Melara <edgarmelara@Edgars-MacBook-Pro.local>
2022-09-11 01:07:29 -0600

    feat: agregar metodo logger en monitor

---
+++ Monitor/tareas.py
@@ -0,0 +1,9 @@
+import datetime
+from celery import Celery
+
+celery_app = Celery(__name__, broker="redis://localhost:6379/0")
+
+@celery_app.task(name='monitor.logger')
+def registrar_log(args):
+    with open('log_boton_panico.txt', '+a') as file:
+        file.write('alerta boton de panico - {}'.format(args))
