854986c52e2e90ac9f06dc9101fee04182732c97
Edgar Melara <edgarmelara@Edgars-MacBook-Pro.local>
2022-09-11 12:16:33 -0600

    fix: agregar linea separada  a metodo de log

--- Monitor/tareas/tareas.py
+++ Monitor/tareas/tareas.py
@@ -6,4 +6,4 @@ celery_app = Celery("tasks", broker="redis://localhost:6379/0")
 @celery_app.task(name='monitor.logger')
 def registrar_log(args):
     with open('log_boton_panico.txt', '+a') as file:
-        file.write('alerta boton de panico - {}'.format(args))
+        file.write('alerta boton de panico - {}\n'.format(args))
