7916fc91ad0c8e008511deb33341f6e3aa44ed10
Luis Solier Sajami <luis.sajami@gmail.com>
2022-09-11 09:43:09 -0500

    Se agrega comunicacion pub/sub redis

--- Mensajeria/mensajeria/mensajes/mensajes.py
+++ Mensajeria/mensajeria/mensajes/mensajes.py
@@ -17,10 +17,10 @@ def informar_estado_salud(solicitud_alerta_json):
                                 nombre_servicio=solicitud_alerta_json['service'])
     #enviar solicitud rest
     db.session.add(estado_salud)
-    frozen = jsonpickle.encode(estado_salud)
-    requests.post(url = 'http://127.0.0.1:5006/monitor', data = frozen)
-    #publish(estado_salud)
+    estado_salud_serializable = jsonpickle.encode(estado_salud)
+    requests.post(url = 'http://127.0.0.1:5006/monitor', data = estado_salud_serializable)
+    publish(estado_salud_serializable)
 
-#def publish(message):
-#    r = redis.Redis(host="redis://localhost")
-#    r.publish("pub", message)
+def publish(message):
+    red = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
+    red.publish('pub', message)
--- Mensajeria/monitor/app.py
+++ Mensajeria/monitor/app.py
@@ -1,7 +1,10 @@
 from monitor import create_app
 from flask_restful import Resource, Api
-from flask import Flask, request
+from flask import Flask, request, make_response, jsonify
+import redis
+import datetime
 import json
+import time
 
 app = create_app('default')
 app_context = app.app_context()
@@ -15,5 +18,17 @@ class VistaMonitor(Resource):
     def post(self):
         print('Monitor is working')
             
+class VistaMonitorSub(Resource):
+
+    def get(self):
+        red = redis.StrictRedis('localhost', 6379, charset="utf-8", decode_responses=True)
+        sub = red.pubsub()
+        sub.subscribe('pub')
+        while True:
+            message = sub.get_message()
+            if message:
+                print(message)
+            time.sleep(0.01)    
 
 api.add_resource(VistaMonitor, '/monitor')
+api.add_resource(VistaMonitorSub, '/monitor-sub')
